22e26831e3dd5fe6ff28add527c13294d770a4ac
Merekeyev <Rayymbek.Merekeev@berekebank.kz>
2026-08-29 00:11:24 +0500

    Initial Commit

---
+++ QarjyFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QarjyFlow
+//
+//  Created by Raim on 29.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QarjyFlow/QarjyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  QarjyFlowApp.swift
+//  QarjyFlow
+//
+//  Created by Raim on 29.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct QarjyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
